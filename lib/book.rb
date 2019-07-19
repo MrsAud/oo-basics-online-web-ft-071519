77582bd74a790ab16389450cbf6c6759ac8f674c
Audrea Cook <audreacwork@gmail.com>
2019-07-19 13:10:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -13,15 +13,15 @@ class Book
 end
 
 ### prediction
- book = Book.new("Harry Potter") 
- puts book.title #=> "Harry Potter"
- book.author = "J.K. Rowling"
- puts book.author
- book.page_count = 4224
- puts book.page_count #=> 4224
- book.genre = "fantasy"
- puts book.genre #=> "fantasy"
- book.turn_page #=> "Flipping the page...wow, you read fast!"
+ # book = Book.new("Harry Potter") 
+ # puts book.title #=> "Harry Potter"
+ # book.author = "J.K. Rowling"
+ # puts book.author
+ # book.page_count = 4224
+ # puts book.page_count #=> 4224
+ # book.genre = "fantasy"
+ # puts book.genre #=> "fantasy"
+ # book.turn_page #=> "Flipping the page...wow, you read fast!"
 
 
 
